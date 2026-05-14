fac1311fdc93cf64081145908d676df1f0f78afc
garpunk <garpunked@gmail.com>
2026-05-14 10:28:37 -0600

    fixed reference in init-db

--- src/database/init-db.js
+++ src/database/init-db.js
@@ -5,11 +5,11 @@ import { products } from "./products.js";
 const uri = process.env.MONGO_URI || "";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 //database name
@@ -17,77 +17,76 @@ const client = new MongoClient(uri, {
 const databaseName = process.env.MONGO_DATABASE || "";
 //define the init function to connect to our database and create collections
 const init = async () => {
-    try {
-        await client.connect();
-        console.log(`Connected to MongoDB`);
-        // get a reference to the actual database we will be using with .db(<database name>)
-        const db = client.db(databaseName);
+  try {
+    await client.connect();
+    console.log(`Connected to MongoDB`);
+    // get a reference to the actual database we will be using with .db(<database name>)
+    const db = client.db(databaseName);
 
-        // initialize the Products collection
-        await seedProducts(db);
-    } catch (error) {
-        console.error(error.message);
-    } finally {
-        await client.close();
-    }
+    // initialize the Products collection
+    await seedProducts(db);
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    await client.close();
+  }
 };
 
-
 const lowerCaseKeys = function (obj) {
-    // if it is an object, but NOT an array, then we need to iterate through all of its keys
-    if (typeof obj === "object" && !Array.isArray(obj)) {
-        for (let key in obj) {
-            // take the first letter (key[0]) of the key and make it lowercase
-            // then add that to the rest of the key after REMOVING the first letter (key.slice(1))
-            let newKey = key[0].toLowerCase() + key.slice(1);
-            // if the value of this key is an object, then we need to call this function again
-            if (typeof obj[key] === "object") {
-                obj[newKey] = lowerCaseKeys(obj[key]);
-                delete obj[key];
-            } else {
-                obj[newKey] = obj[key];
-                delete obj[key];
-            }
-        }
-    } else if (Array.isArray(obj)) {
-        // if it is an array, then we need to iterate through each item in the array
-        // and for each object value call the function again.
-        for (let i = 0; i < obj.length; i++) {
-            let item = obj[i];
-            if (typeof item === "object") {
-                obj[i] = lowerCaseKeys(item);
-            }
-        }
+  // if it is an object, but NOT an array, then we need to iterate through all of its keys
+  if (typeof obj === "object" && !Array.isArray(obj)) {
+    for (let key in obj) {
+      // take the first letter (key[0]) of the key and make it lowercase
+      // then add that to the rest of the key after REMOVING the first letter (key.slice(1))
+      let newKey = key[0].toLowerCase() + key.slice(1);
+      // if the value of this key is an object, then we need to call this function again
+      if (typeof obj[key] === "object") {
+        obj[newKey] = lowerCaseKeys(obj[key]);
+        delete obj[key];
+      } else {
+        obj[newKey] = obj[key];
+        delete obj[key];
+      }
     }
-    return obj;
+  } else if (Array.isArray(obj)) {
+    // if it is an array, then we need to iterate through each item in the array
+    // and for each object value call the function again.
+    for (let i = 0; i < obj.length; i++) {
+      let item = obj[i];
+      if (typeof item === "object") {
+        obj[i] = lowerCaseKeys(item);
+      }
+    }
+  }
+  return obj;
 };
 
 const seedProducts = async (db) => {
-    // we need to make a small transform to the provided data before inserting
-    // use .map() to transform each product before inserting it into the database
-    // change Reviews.ReviewUrl to match the following pattern: /products/<productId>/reviews/
+  // we need to make a small transform to the provided data before inserting
+  // use .map() to transform each product before inserting it into the database
+  // change Reviews.ReviewUrl to match the following pattern: /products/<productId>/reviews/
 
-    // while we are at it...the data provided used a PascalCase naming convention for its keys. Use the provided lowerCaseKeys function to convert all keys to camelCase. This will make it consistent with the rest of our models.
+  // while we are at it...the data provided used a PascalCase naming convention for its keys. Use the provided lowerCaseKeys function to convert all keys to camelCase. This will make it consistent with the rest of our models.
 
-    try {
-        // drop the collection to clear out the old records
-        await db.products.drop();
+  try {
+    // drop the collection to clear out the old records
+    await db.collection("products").drop();
 
-        console.log("Collection 'products' dropped successfully");
-        // create a new collection
-        await db.createCollection("products")
+    console.log("Collection 'products' dropped successfully");
+    // create a new collection
+    await db.createCollection("products");
 
-        console.log("Collection 'products' created successfully");
-        // insert all products
-        const result = await db.products.insertMany(products);
+    console.log("Collection 'products' created successfully");
+    // insert all products
+    const result = await db.collection("products").insertMany(products);
 
-        console.log(
-            `${result.insertedCount} new listing(s) created with the following id(s):`,
-        );
-        console.log(result.insertedIds);
-    } catch (error) {
-        console.error(error.message);
-    }
+    console.log(
+      `${result.insertedCount} new listing(s) created with the following id(s):`,
+    );
+    console.log(result.insertedIds);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 init();
